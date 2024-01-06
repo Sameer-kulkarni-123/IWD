274de070e74bc5d2156dfdf69753a6f15696d946
Sameer-kulkarni-123 <148962321+Sameer-kulkarni-123@users.noreply.github.com>
2024-01-06 09:32:21 +0530

    Add files via upload

---
+++ Main_js.js
@@ -0,0 +1,7 @@
+function promptForPhoneNumber() {
+  var userPhoneName = prompt("Please enter your name");
+  var userPhoneNumber = prompt("Please enter your Email:");
+  if (userPhoneNumber !== null) {
+      alert("Thank you! ");
+  }
+}
